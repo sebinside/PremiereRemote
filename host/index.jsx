30d191a3734f5e94154216d26222955ad5bceb2b
sebinside <hi@sebinside.de>
2018-10-05 21:47:21 +0200

    Added first own custom CEP functions.

--- host/index.jsx
+++ host/index.jsx
@@ -1,11 +1,55 @@
 var host = {
-    projectNameAlert: function () {
-        alert(app.project.name);
-    },
-    alert: function (content) {
-        alert("Alert: " + content);
-    },
-    kill: function() {
+    kill: function () {
         // This method is only there for debugging purposes.
+    },
+    muteAudioTrack: function (trackNumber) { // 0 based
+        trackNumber = parseInt(trackNumber);
+
+        const sequence = app.project.activeSequence;
+
+        if (trackNumber < sequence.audioTracks.numTracks) {
+            sequence.audioTracks[trackNumber].setMute();
+        }
+    },
+    selectSelectedNamedVideoLayer: function (name) {
+
+        var sequence = app.project.activeSequence;
+
+        if (sequence) {
+
+            // Search all selected video layers
+            for (var i = 0; i < sequence.videoTracks.numTracks; i++) {
+
+                var allVideoClips = sequence.videoTracks[i].clips;
+                for (var n = 0; n < allVideoClips.numItems; n++) {
+
+                    var clip = allVideoClips[n];
+
+                    if (clip.isSelected()) {
+
+                        // Maybe, this is the correctly named layer
+                        if (clip.name == name) {
+                            clip.setSelected(true, true);
+                        } else {
+                            clip.setSelected(false, true);
+                        }
+                    }
+                }
+            }
+
+            // Deselect all audio layers
+            for (var i = 0; i < sequence.audioTracks.numTracks; i++) {
+
+                var allAudioClips = sequence.audioTracks[i].clips;
+
+                for (var n = 0; n < allAudioClips.numItems; n++) {
+
+                    var clip = allAudioClips[n];
+                    clip.setSelected(false, true);
+
+                }
+            }
+        }
     }
+
 };
