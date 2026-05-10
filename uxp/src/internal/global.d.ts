81c21f3ae432db11b309374bbd70b6d83b474e2b
Dr.-Ing. Sebastian Hahner <hi@sebinside.de>
2026-05-10 23:47:24 +0200

    Remove global types.

--- uxp/src/internal/global.d.ts
+++
@@ -1,3 +0,0 @@
-// UXP injects require() as a global even in ES module context.
-// Used to access UXP-native modules like "premierepro" that are not ES modules.
-declare function require<T = unknown>(module: string): T;
